642e7a35a142b42b8911ffb7db9cfa8d0737b5ad
Rodrigo Viana <rodrigovianaprado@gmail.com>
2017-01-13 15:50:36 -0200

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const	
+	express = require('express'),
+	bodyParser = require('body-parser'),
+	csv=require('csvtojson'),
+	multiparty = require('connect-multiparty'),
+	ulpload = require('./routes/upload'),	
+	app = express();
+
+const
+	port = process.env.PORT || 8080;  	
+	app.use(bodyParser.urlencoded({ extended: true }));
+  	app.use(bodyParser.json());
+  	app.listen(port);
+
+ 	let	csvFilePath='./uploads/some-csv-file.csv';
+
+  	//Lendo arquivo csv
+	csv()
+	.fromFile(csvFilePath)
+	.on('json',(jsonObj)=>{
+   	console.log(jsonObj);
+	})
+	.on('done',(error)=>{
+    console.log('end')
+	});
+
+	//Rotas
+	app.post('/api/upload',multiparty(), ulpload.recebe);
---
+++ routes/upload.js
@@ -0,0 +1,50 @@
+const
+	fs = require('fs'),
+	csv=require('csvtojson');
+  
+module.exports.recebe = _recebe;
+
+
+function _recebe(req, res){
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  let arquivo = req.files.file;
+  let temporario = req.files.file.path;
+  let novo = './uploads/' + concatDateTime() + '.csv';
+  
+ 	fs.rename(temporario, novo, function(err){
+ 		readCSV(novo); 		
+ 		if(err){
+ 			res.status(500).json({error: err})
+ 		}
+ 		res.json({message: "enviado com sucesso.", file: novo});
+ 	})
+}
+
+
+
+function concatDateTime(){
+	let date = new Date();  
+  	let day = date.getDate();
+  	let monthIndex = date.getMonth() + 1;
+  	let year = date.getFullYear();
+  	let minutos = date.getMinutes();
+  	let horas = date.getHours();
+  
+  	let dateConcat = `${year}_${monthIndex}_${day}-${horas}_${minutos}`;
+
+  	return dateConcat;
+};
+
+function readCSV(caminhoArquivo){
+	let	csvFilePath = caminhoArquivo; 
+
+  	//Lendo arquivo csv
+	csv()
+	.fromFile(csvFilePath)
+	.on('json',(jsonObj)=>{
+   	console.log(jsonObj);
+	})
+	.on('done',(error)=>{
+    console.log('end')
+	});
+}
